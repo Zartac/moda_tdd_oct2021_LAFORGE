be48baf74b10554d52fd078b24fc09ea8219ba05
Zartac <laforgejohan7@gmail.com>
2021-10-28 11:15:38 +0200

    up-to-date

--- app/src/test/java/moda_tdd_oct2021_LAFORGE/DictionaryTest.java
+++ app/src/test/java/moda_tdd_oct2021_LAFORGE/DictionaryTest.java
@@ -6,8 +6,9 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 public class DictionaryTest {
+    Dictionary dict = new Dictionary("Example");
     @Test
-    public void testDictionaryName(Dictionary dict) {
+    public void testDictionaryName() {
         assertThat(dict.getName(), equalTo("Example"));
     }
 }
