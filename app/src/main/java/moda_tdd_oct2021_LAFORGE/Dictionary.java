6b80a8b5f7c16eb64d24834e7149c559cccfdcf8
Zartac <laforgejohan7@gmail.com>
2021-10-28 12:33:50 +0200

    up-to-date

--- app/src/main/java/moda_tdd_oct2021_LAFORGE/Dictionary.java
+++ app/src/main/java/moda_tdd_oct2021_LAFORGE/Dictionary.java
@@ -1,16 +1,32 @@
 package moda_tdd_oct2021_LAFORGE;
 
+import java.util.HashMap;
+import java.util.*;
+
 public class Dictionary {
-    private String name;
+    private Map<String, String> translations;
+    private  String name;
     public Dictionary(String name){
         this.name=name;
+        translations = new HashMap<>();
+        translations.put(name, null);
     }
-
+    public Dictionary(){
+        translations = new HashMap<>();
+    }
     public String getName(){
         return this.name;
     }
     public boolean isEmpty(){
-        if(this!=null) return false;
-        return true;
+        return this.name.isEmpty();
+    }
+
+    public String getTranslation(String name) {
+        for(String s : translations.keySet()) if(s.equals(name)) return this.translations.get(s) ;
+        return name;
+    }
+
+    public void addTranslation(String name, String translated) {
+        this.translations.put(name, translated);
     }
 }
--- app/src/test/java/moda_tdd_oct2021_LAFORGE/DictionaryTest.java
+++ app/src/test/java/moda_tdd_oct2021_LAFORGE/DictionaryTest.java
@@ -1,5 +1,6 @@
 package moda_tdd_oct2021_LAFORGE;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -7,6 +8,7 @@ import static org.hamcrest.Matchers.*;
 
 public class DictionaryTest {
     Dictionary dict = new Dictionary("Example");
+    Dictionary waiter = new Dictionary();
     @Test
     public void testDictionaryName() {
         assertThat(dict.getName(), equalTo("Example"));
@@ -14,5 +16,11 @@ public class DictionaryTest {
     @Test
     public void testDictionaryNotEmpty(){
         assertThat(dict.isEmpty(), equalTo(false));
+    }
+    @Test public void testOneTranslation() {
+        dict.addTranslation("contre", "against");
+        dict.addTranslation("pour","for");
+        assertThat(dict.getTranslation("contre"), equalTo("against"));
+        assertThat(dict.getTranslation("pour"), not(equalTo("against")));
     }
 }
